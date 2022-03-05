b3b57b857ec272b84bdd52e3f2360b3ce3dad20a
quanglinh304 <linh.nq3004@gmail.com>
2022-03-05 20:40:01 +0700

    a

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,11 +3,11 @@ import HomeList from '../component/home/HomeList'
 import 'antd/dist/antd.css';
 import './routes.css';
 import { Layout, Breadcrumb } from 'antd';
-import SiderBar from "../component/common/leftsidebar/leftsidebar";
+import LeftSideBar from "../component/common/leftsidebar/leftsidebar";
 import CustomerList from "../component/customer/CustomerList";
 
 const { Content } = Layout;
-
+ 
 const Routes = () => {
 
   const menuItems = [
@@ -18,7 +18,7 @@ const Routes = () => {
   return (
     <Switch>
       <Layout className="layout">
-        <SiderBar menuItems={menuItems}/>
+        <LeftSideBar menuItems={menuItems}/>
         <Content style={{ padding: "30px 50px" }}>
           <div className="site-layout-content">
             <Route path={"/"} exact component={HomeList} />
